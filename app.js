72c7e06112cda45405288ce6f887bc9624f1e22b
2011domainbot <2011domainbot@gmail.com>
2022-02-07 19:23:04 -0700

    Fix subtraction to be last 5 minutes | 300 seconds

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ const assetQuery = async(tokenId, address) => {
 
 // Poll OpenSea every 5 minutes & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
 setInterval(() => {
-  const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
+  const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(300, "seconds").unix();
 
   console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
 
