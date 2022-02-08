f5732423f6067eb6b91395964b378f1dbe9eb216
2011domainbot <2011domainbot@gmail.com>
2022-02-07 20:27:30 -0700

    Update hashtags

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function formatAndSendTweet(event) {
   const formattedEthPrice = formattedUnits * tokenEthPrice;
   const formattedUsdPrice = formattedUnits * tokenUsdPrice;
 
-  const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #2011 #Namecoin ${openseaLink}`;
+  const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #2011 #Namecoin #NFT ${openseaLink}`;
 
   console.log(tweetText);
 
